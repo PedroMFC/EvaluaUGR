68a49935bfba861925aba074574d1ad8facaba20
Pedro Flores <pedro_23_96@hotmail.com>
2020-10-25 15:24:52 +0100

    #26 añadida funcion para conexion de base de datos

---
+++ internal/microVal/models/setup.go
@@ -0,0 +1,32 @@
+package models
+
+import (
+	"github.com/jinzhu/gorm"
+	//_ "github.com/jinzhu/gorm/dialects/postgres"
+)
+
+
+//ValoracionBD es la encargada de realizar las operaciones sobre la base de datos
+type ValoracionBD struct{ 
+	DB *gorm.DB
+} 
+
+var baseDatos ValoracionBD
+
+//ConnectDatabase sirve para conectarse a la base de datos de valoraciones
+func (vDB *ValoracionBD) ConnectDatabase() {
+	
+}
+
+//ValoracionDB implementa la interfaz IManjeValoraciones
+func (vDB *ValoracionBD) valorarAsigantura(asignatura string, opinion int){
+	
+}
+
+func (vDB *ValoracionBD) verValoraciones(asignatura string){
+	
+}
+
+func (vDB *ValoracionBD) verValoracionMedia(asignaura string){
+	
+}
