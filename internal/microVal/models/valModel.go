3fbd4a25d4dcf6902639ebed7b082e9327dcfbfd
Pedro Flores <pedro_23_96@hotmail.com>
2020-10-25 15:23:40 +0100

    Añadida interfaz operaciones de valoraciones closes #27

--- internal/microVal/models/valModel.go
+++ internal/microVal/models/valModel.go
@@ -1,7 +1,15 @@
 package models
 
+//Valoracion contiene información acerca de los datos de una asignatura
 type Valoracion struct {
 	ID     uint   `json:"id" gorm:"primary_key"`
 	Asignatura  string `json:"asignatura"`
 	Valoracion int `json:"valoracion"`
+}
+
+//IManejoValoraciones se encarga de implementar todas las operaciones sobre la base de datos con las valoraciones
+type IManejoValoraciones interface{
+	valorarAsigantura(asignatura string, opinion int)
+	verValoraciones(asignatura string)
+	verValoracionMedia(asignaura string)
 }
