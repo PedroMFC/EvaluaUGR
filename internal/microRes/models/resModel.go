11cffa2a02d7d47985641a8aefc85fcf2b6710ba
Pedro Flores <pedro_23_96@hotmail.com>
2020-10-25 17:03:24 +0100

    close #39 añadidos campos valoracion de reseña

--- internal/microRes/models/resModel.go
+++ internal/microRes/models/resModel.go
@@ -5,4 +5,6 @@ type Resenia struct {
 	ID         uint   `json:"id" gorm:"primary_key"`
 	Asignatura string `json:"asignatura"`
 	Opinion    string `json:"opinion"`
+	MeGusta    int    `json:"megusta"`
+	NoMeGusta  int    `json:"nomegusta"`
 }
