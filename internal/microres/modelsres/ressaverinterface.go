57282da6b12410a08ec7a7b370d8300eaafb3f95
Pedro Flores <pedro_23_96@hotmail.com>
2020-12-07 22:55:19 +0100

    #71 creada interfaz para las reseñas

---
+++ internal/microres/modelsres/ressaverinterface.go
@@ -0,0 +1,11 @@
+package modelsres
+
+// $> mockery -dir internal/microres/modelsres -name IResSaver
+
+//IResSaver es la interfaz para las estructuras de datos de las reseñas
+type IResSaver interface {
+	GuardarResenia(asignatura string, opinion *Resenia)
+	ObtenerResenias(asignatura string) []Resenia
+	MeGustaResenia(asignatura string, id int) error
+	NoMeGustaResenia(asignatura string, id int) error
+}
