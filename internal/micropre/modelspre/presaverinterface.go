f82471f0bd23a59bdfa7cfd951a421f01c04c0dd
Pedro Flores <pedro_23_96@hotmail.com>
2020-12-08 11:42:09 +0100

    #72 Añadida interfaz para almacenar preguntas

---
+++ internal/micropre/modelspre/presaverinterface.go
@@ -0,0 +1,10 @@
+package modelspre
+
+// $> mockery -dir internal/micropre/modelspre -name IPreSaver
+
+//IPreSaver define las operaciones sobre la estructura de datos de preguntas
+type IPreSaver interface {
+	GuardarPregunta(asignatura string, pre *Pregunta)
+	ObtenerPregunta(asignatura string) []Pregunta
+	Responder(asignatura string, id int, res *Respuesta) error
+}
