258489fb93898780e5abaa6263b6d1d2e9e1d262
Pedro Flores <pedro_23_96@hotmail.com>
2021-01-12 17:10:59 +0100

    Añadido etcd #74

---
+++ cmd/server/routes.go
@@ -0,0 +1,111 @@
+package server
+
+import (
+	log "github.com/sirupsen/logrus"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+
+	"github.com/PedroMFC/EvaluaUGR/internal/microval/modelsval"
+
+	"go.etcd.io/etcd/clientv3"
+	"golang.org/x/net/context"
+	"github.com/joho/godotenv"
+	"os"
+	"time"
+)
+
+/*Singleton of Truth*/
+
+var ValRepo modelsval.ValoracionRepositorio
+var ValMap  modelsval.ValoracionMap
+func StartData(){
+	ValMap = *modelsval.NewValoracionMap()
+	val := new(modelsval.Valoracion)
+	val2 := new(modelsval.Valoracion)
+	val.Valoracion = 2
+	val2.Valoracion = 3
+	ValMap.Valoraciones["AAA"] = []modelsval.Valoracion{*val, *val2}
+	ValRepo = *modelsval.NewValoracionsRepositorio(&ValMap)
+}
+
+type applicationGin struct {
+	Router *gin.Engine
+}
+
+
+// NO usamos esta función por ahora
+// La definimos con la configuración distribuida etcd
+// pero no llegamos a arrancar el microservicio
+func (a *applicationGin) Start(port string) {
+	const PortVarName = "PUERTO"
+
+	// Conectamos con el cliente de etcd
+	cli, err := clientv3.New(clientv3.Config{
+		Endpoints:   []string{"localhost:2379", "localhost:22379", "localhost:32379"},
+		DialTimeout: 5 * time.Second,
+	})
+
+	// Vemos que hayamos podido hacerlo
+	if err != nil{
+		log.Fatal("No se ha podido conectar con el cliente", err)
+	}
+
+	// Si hemos introducido un puerto directamente empezamos a trabajar en ese puerto
+	// Almacenamos en etcd
+	if port!= ""{
+		ctx, _ := context.WithTimeout(context.Background(), 5 * time.Second)
+		_, err := cli.Put(ctx, PortVarName, port) 
+
+		if err != nil{
+			log.Fatal("No se ha podido escribir la clave", err)
+		}
+	// Si no, leemos desde el archivo .env si existe
+	} else{ 
+		err := godotenv.Load()
+
+		if err != nil {
+			log.Println("Error loading .env file")
+		}
+
+		// Si hay una entrada en el .env
+		//Almacenamos en etcd
+		port = os.Getenv(PortVarName); 
+		log.Println("Puerto del .env" + port)
+		if port != ""{
+			log.Println("Usamos la configuración del .env")
+			ctx, _ := context.WithTimeout(context.Background(), 5 * time.Second)
+			_, err := cli.Put(ctx, PortVarName, port)
+
+			if err != nil{
+				log.Fatal("No se ha podido escribir la clave", err)
+			}
+		// Si las anteriores han fallado, usamos un puerto por defecto
+		//Almacenamos en etcd
+		} else{
+			log.Println("Usamos la configuración por defecto")
+			port = "8080"
+			ctx, _ := context.WithTimeout(context.Background(), 5 * time.Second)
+			_, err := cli.Put(ctx, PortVarName, port)
+
+			if err != nil{
+				log.Fatal("No se ha podido escribir la clave", err)
+			}
+		}
+
+	}
+
+	log.Println("GIN valoraciones conectado en " + port)
+	log.Fatal(http.ListenAndServe("localhost:" + port, a.Router))
+}
+
+
+func NewAppGin() *applicationGin {
+	router := gin.Default()
+
+	//Aquí definimos las rutas
+
+
+	return &applicationGin{Router: router}
+}
+
--- go.mod
+++ go.mod
@@ -6,13 +6,15 @@ require (
 	github.com/gin-gonic/gin v1.6.3
 	github.com/golang/protobuf v1.4.2 // indirect
 	github.com/gorilla/mux v1.8.0
+	github.com/joho/godotenv v1.3.0
 	github.com/kr/pretty v0.1.0 // indirect
 	github.com/labstack/echo/v4 v4.1.17
 	github.com/pytimer/mux-logrus v0.0.0-20200505085744-ce5a5e748151
 	github.com/rivo/uniseg v0.1.0
 	github.com/sirupsen/logrus v1.7.0
 	github.com/stretchr/testify v1.6.1
-	golang.org/x/net v0.0.0-20201224014010-6772e930b67b // indirect
+	go.etcd.io/etcd v0.5.0-alpha.5.0.20201125193152-8a03d2e9614b
+	golang.org/x/net v0.0.0-20201224014010-6772e930b67b
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	google.golang.org/protobuf v1.25.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
